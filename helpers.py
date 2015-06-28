8851ca0a747aecf079f4a5aa1c61b44b7a77103f
John Robinson <soco@uw.edu>
2015-06-28 15:07:20 -0700

    move convertRFC822ToDateTime to utilities

--- helpers.py
+++ helpers.py
@@ -4,6 +4,26 @@
 import MySQLdb
 from mysql_insert_queries import *
 from pprint import pprint
+import email.utils
+from datetime import datetime
+
+
+#
+# convert an RFC822 date to a datetime
+# borrowed and modified from: https://gist.github.com/robertklep/2928188
+#
+# original used fromtimestamp instead of utcfromtimestamp, which converts it to local time.
+# currently, all of the tweets seem to just use utc, so we can ignore the timestamp (though
+# it's unclear if mktime_tz actually utilizes the info or just ignores it. )
+#
+def convertRFC822ToDateTime(rfc822string):
+	"""
+		convert an RFC822 date to a datetime
+	"""
+	return datetime.utcfromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(rfc822string)))
+
+
+
 
 #
 #
--- normal_import.py
+++ normal_import.py
@@ -4,7 +4,6 @@
 import MySQLdb
 import simplejson as json
 import io
-import email.utils
 from datetime import datetime
 from pprint import pprint
 import os
@@ -12,23 +11,8 @@ import argparse
 import getpass
 
 from mysql_insert_queries import *
-from helpers import *
 
 
-
-#
-# convert an RFC822 date to a datetime
-# borrowed and modified from: https://gist.github.com/robertklep/2928188
-#
-# original used fromtimestamp instead of utcfromtimestamp, which converts it to local time.
-# currently, all of the tweets seem to just use utc, so we can ignore the timestamp (though
-# it's unclear if mktime_tz actually utilizes the info or just ignores it. )
-#
-def convertRFC822ToDateTime(rfc822string):
-	"""
-		convert an RFC822 date to a datetime
-	"""
-	return datetime.utcfromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(rfc822string)))
 
 
 
