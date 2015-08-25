2ec2e7d318f8f82cc7f172cec5a3ad94678e7097
John Robinson <soco@uw.edu>
2015-08-25 05:43:48 +0000

    make into a proper module style

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='twitter_mysql',
+	version='0.1',
+	description='Scripts for somewhat normalizing a twitter dataset into mysql',
+	author='John Robinson',
+	author_email='soco@uw.edu',
+	url='https://www.geosoco.com/projects/twitter-mysql/',
+	packages=['twitter_mysql']
+)
