5f75271ef4c330254b0e5da84116ef146c95e2bd
John Robinson <soco@uw.edu>
2015-08-27 08:47:14 +0000

    add unfinished 3rd set of the data

--- sql/oso3_schema_create3.sql
+++ sql/oso3_schema_create3.sql
@@ -40,6 +40,67 @@ CREATE TABLE `users_current` (
   `deletion_noticed_date` datetime NULL DEFAULT NULL,
 
   PRIMARY KEY (`id`),
+  FOREIGN KEY (`id`) REFERENCES users(`id`),
   KEY `idx_user_screen_name` (`screen_name`),
-  FOREIGN KEY (`source_snapshot_id`) REFERENCES tweet_snapshots(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `user_stats` (
+  `id` bigint NOT NULL,
+
+  `statuses_count_in_set` int DEFAULT NULL, 
+
+  `followers_count_min` int DEFAULT NULL,
+  `followers_count_max` int DEFAULT NULL,
+  `followers_count_delta` int DEFAULT NULL,
+  `friends_count_min` int DEFAULT NULL,
+  `friends_count_max` int DEFAULT NULL,
+  `friends_count_delta` int DEFAULT NULL,
+  `statuses_count_min` int DEFAULT NULL,
+  `statuses_count_max` int DEFAULT NULL,
+  `statuses_count_delta` int DEFAULT NULL,
+  `favourites_count_min` int DEFAULT NULL,
+  `favourites_count_max` int DEFAULT NULL,
+  `favourites_count_delta` int DEFAULT NULL,
+  `listed_count_min` int DEFAULT NULL,
+  `listed_count_max` int DEFAULT NULL,
+  `listed_count_delta` int DEFAULT NULL,
+
+  `screen_name_change_count` int DEFAULT NULL,
+  `name_change_count` int DEFAULT NULL,
+  `location_change_count` int DEFAULT NULL,
+  `geo_enabled_change_count` int DEFAULT NULL,
+  `description_change_count` int DEFAULT NULL,
+  `profile_change_count` int DEFAULT NULL,
+  `url_change_count` int DEFAULT NULL,
+
+  /* in set values */
+  `timeline_total_retweets` int DEFAULT NULL, /* number of user's tweets in set that have been retweeted */
+  `timeline_total_replied` int DEFAULT NULL, /* number of tweets that have replies from other people */
+  `timeline_total_favorited` int DEFAULT NULL, /* number of tweets that have been favorited */
+
+  `users_retweet_count` int DEFAULT NULL, /* number of users tweets in collection that are retweets */
+  `users_replies_count` int DEFAULT NULL, /* number of uers tweets in collection that are replies */
+
+  `percent_tweets_retweets` int DEFAULT NULL, /* % of users's tweets that are retweets */
+  `percent_tweets_replies` int DEFAULT NULL, /* % of user's tweets that are replies */
+  `percent_tweets_originals` int DEFAULT NULL, /* number of tweets that are deemed to be original */
+
+
+
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id`) REFERENCES users(`id`),
+
+)
+
+
+CREATE TABLE `tweet_stats` (
+  `id` bigint NOT NULL,
+
+  `replies_count` int DEFAULT NULL,
+  `replies_in_set` int DEFAULT NULL
+
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id`) REFERENCES tweets(`id`),
+
+)
