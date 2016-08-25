92fec9232a42392d77a41fa659b84d2bf18608f7
John Robinson <soco@uw.edu>
2016-08-25 11:33:19 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import uuid
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+reqs = [str(req.req) for req in install_reqs]
+
 
 setup(
-	name='twitter_mysql',
-	version='0.1',
-	description='Scripts for somewhat normalizing a twitter dataset into mysql',
-	author='John Robinson',
-	author_email='soco@uw.edu',
-	url='https://www.geosoco.com/projects/twitter-mysql/',
-	packages=['twitter_mysql']
+    name='twitter_mysql',
+    version='0.1',
+    description='Scripts for normalizing a twitter dataset into mysql',
+    author='John Robinson',
+    author_email='soco@uw.edu',
+    url='https://www.geosoco.com/projects/twitter-mysql/',
+    packages=['twitter_mysql'],
+    install_requires=reqs,
 )
